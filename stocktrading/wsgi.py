f3d84aa498472c20662a00c83f4e94c515a05b94
Peter Uythoven <puythoven72@gmail.com>
2022-10-05 17:34:38 -0400

    found a spelling error in wsgi.py

--- stocktrading/wsgi.py
+++ stocktrading/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stocktradeing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stocktrading.settings')
 
 application = get_wsgi_application()
