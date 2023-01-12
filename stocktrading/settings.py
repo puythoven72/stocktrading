72e1b44f33a1676a90bcdb6c87687842a2690248
Peter Uythoven <puythoven72@gmail.com>
2023-01-12 15:15:40 -0500

    updateing settings csrf_trusted_origins

--- stocktrading/settings.py
+++ stocktrading/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://stocktrading-production.up.railway.app/',] 
+CSRF_TRUSTED_ORIGINS = ['https://stocktrading-production.up.railway.app',]  
 
 
 # Application definition
